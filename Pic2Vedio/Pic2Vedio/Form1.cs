b0e48caf48c3f895dcaa12f59211d143be452c07
dexterlivegithub <dexerlive@live.cn>
2013-05-08 10:57:39 +0800

    //-----

--- Pic2Vedio/Pic2Vedio/Form1.cs
+++ Pic2Vedio/Pic2Vedio/Form1.cs
@@ -31,13 +31,13 @@ namespace Pic2Vedio
             if (basfile != null)
             {
                 Bitmap bp = new Bitmap(basfile.FullName);
-                FontFamily f=new FontFamily("宋体");
-                
-                for (int i = 0; i <200; i++)
+                FontFamily f = new FontFamily("宋体");
+
+                for (int i = 0; i < 200; i++)
                 {
-                    Bitmap tmp = new Bitmap(bp);                  
-                   Image img = wm.Mark(tmp, MarkType.Text, string.Format( "{0},{1}",i,(int) (i/24)), tmp, 150, 150, true, Color.Red, 0F, f);
-                   img.Save("./demo/" + i + ".jpg");
+                    Bitmap tmp = new Bitmap(bp);
+                    Image img = wm.Mark(tmp, MarkType.Text, string.Format("{0},{1}", i, (int)(i / 24)), tmp, 150, 150, true, Color.Red, 0F, f);
+                    img.Save("./demo/" + i + ".jpg");
                 }
             }
 
@@ -45,184 +45,104 @@ namespace Pic2Vedio
 
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenFileDialog dig = new OpenFileDialog();
-            dig.Multiselect = true;
-            dig.Filter = "图片|*.jpg;*.png;*.gif";
-            // generates a little slide-show, with audio track and fades between images.
-            if(dig.ShowDialog()== System.Windows.Forms.DialogResult.OK )
+            try
             {
 
-                RunBuilder(dig.FileNames, this.fps.Value).ContinueWith((r) =>
+
+
+                OpenFileDialog dig = new OpenFileDialog();
+                dig.Multiselect = true;
+                dig.Filter = "图片|*.jpg;*.png;*.gif";
+                // generates a little slide-show, with audio track and fades between images.
+                if (dig.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                 {
-                MessageBox.Show("Test");
-            });
+
+                    string[] files = dig.FileNames;
+                    double fr = this.fps.Value;
+                    RunBuilder(dig.FileNames, this.fps.Value);
+                    MessageBox.Show("Completed");
+
+
+                }
+
+
 
             }
-            #region old version
-            /*
-            string outputFile = "FadeBetweenImages.wmv";
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+
+        }
 
 
+
+        private void RunBuilder(string[] files, double fr)
+        {
+
+
+            #region v1
+            // generates a little slide-show, with audio track and fades between images.
+            string path = Properties.Settings.Default.OutPut;
+            if (string.IsNullOrEmpty(path))
+            {
+                path = AppDomain.CurrentDomain.BaseDirectory;
+            }
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string outputFile = path + "\\demo" + DateTime.Now.ToString("yyyyMMdd") + ".wmv";
+
+            double fps = ((double)1) / Convert.ToDouble(fr);
 
             using (ITimeline timeline = new DefaultTimeline())
             {
+                Bitmap top = new Bitmap(files[0]);
 
-                IGroup group = timeline.AddVideoGroup(32, 2560, 1920);
-
-                //timeline
-                double halfDuration = 0.5;
+                IGroup group = timeline.AddVideoGroup(32, top.Width, top.Height);
 
                 ITrack videoTrack = group.AddTrack();
 
-                // IClip clip1 = videoTrack.AddImage("image1.jpg", 0, 2); // play first image for a little while
-                System.IO.DirectoryInfo dirinfo = new System.IO.DirectoryInfo("./demo/");
-                int idx = 0;
-                if (dirinfo != null)
+
+                // IOrderedEnumerable<FileInfo> enumerable = JPGfilesCopy.OrderBy(f => f.CreationTime);
+
+                foreach (string f in files)
                 {
-                    FileInfo[] files = dirinfo.GetFiles("*.jpg");
-                    foreach (var item in files)
-                    {
-                        //System.Drawing.Bitmap b = new Bitmap(item.FullName);
-                        IClip clip1 = videoTrack.AddImage(item.FullName, idx++ * 1, idx * 1);
-                        group.AddTransition(clip1.Offset - halfDuration, halfDuration, StandardTransitions.CreateFade(), true);
+                    //listBox1.Items.Add(f.FullName);
+                    IClip k = videoTrack.AddImage(f, 0, fps);
 
-                        group.AddTransition(clip1.Offset, halfDuration, StandardTransitions.CreateFade(), false);
-                    }
                 }
-
-                //IClip clip2 = videoTrack.AddImage("image2.jpg", 0, 2); // and the next
-
-                //IClip clip3 = videoTrack.AddImage("image3.jpg", 0, 2); // and finally the last
-
-                //IClip clip4 = videoTrack.AddImage("image4.jpg", 0, 2); // and finally the last
-
-
-
-                //double halfDuration = 0.5;
-
-
-
-                //// fade out and back in
-
-                //group.AddTransition(clip2.Offset - halfDuration, halfDuration, StandardTransitions.CreateFade(), true);
-
-                //group.AddTransition(clip2.Offset, halfDuration, StandardTransitions.CreateFade(), false);
-
-
-
-                //// again
-
-                //group.AddTransition(clip3.Offset - halfDuration, halfDuration, StandardTransitions.CreateFade(), true);
-
-                //group.AddTransition(clip3.Offset, halfDuration, StandardTransitions.CreateFade(), false);
-
-
-
-                //// and again
-
-                //group.AddTransition(clip4.Offset - halfDuration, halfDuration, StandardTransitions.CreateFade(), true);
-
-                //group.AddTransition(clip4.Offset, halfDuration, StandardTransitions.CreateFade(), false);
-
-
 
                 // add some audio
 
                 ITrack audioTrack = timeline.AddAudioGroup().AddTrack();
+                //ITrack audioTrack = timeline.AddAudioGroup().AddTrack();
 
-
-
-                // IClip audio = audioTrack.AddAudio("testinput.wav", 0, videoTrack.Duration);
-
-
-
-                //// create an audio envelope effect, this will:
-
-                //// fade the audio from 0% to 100% in 1 second.
-
-                //// play at full volume until 1 second before the end of the track
-
-                //// fade back out to 0% volume
-
-                //audioTrack.AddEffect(0, audio.Duration,
-
-                //               StandardEffects.CreateAudioEnvelope(1.0, 1.0, 1.0, audio.Duration));
-
-
+                //IClip audio = audioTrack.AddAudio(@textBox1.Text, 0, videoTrack.Duration);
 
                 // render our slideshow out to a windows media file
+                //string path = Environment.GetFolderPath(Environment.SpecialFolder.Programs);
 
-                //using (
-
-                IRenderer renderer =
-
-                   new WindowsMediaRenderer(timeline, outputFile, WindowsMediaProfiles.HighQualityVideo);
+                using (WindowsMediaRenderer renderer = new WindowsMediaRenderer(timeline, outputFile, WindowsMediaProfiles.HighQualityVideo))
                 {
-
                     renderer.Render();
 
                 }
-              }
-            */
+
+                //string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                // axWindowsMediaPlayer1.URL = @"C:\Users\dell\Documents\My Dropbox\Deema\Deema\bin\Release\" + outputFile;
+
+            }
             #endregion
+            // });
 
-           
-            
-
-        }
-
-        private Task RunBuilder(string[] files,double fr )
-        {
-            return Task.Run(() => {
-
-                #region v1
-                // generates a little slide-show, with audio track and fades between images.
-
-                string outputFile = "demo"+ DateTime.Now.ToString("yyyyMMdd")+".wmv";
-
-                double fps =  ((double)1) / Convert.ToDouble(fr);
-
-                using (ITimeline timeline = new DefaultTimeline())
-                {
-                    Bitmap top = new Bitmap(files[0]);
-
-                    IGroup group = timeline.AddVideoGroup(32, top.Width, top.Height);
-
-                    ITrack videoTrack = group.AddTrack();
-
-
-                   // IOrderedEnumerable<FileInfo> enumerable = JPGfilesCopy.OrderBy(f => f.CreationTime);
-
-                    foreach (string  f in files)
-                    {
-                        //listBox1.Items.Add(f.FullName);
-                        IClip k = videoTrack.AddImage(f, 0, fps);
-
-                    }
-
-                    // add some audio
-
-                    ITrack audioTrack = timeline.AddAudioGroup().AddTrack();
-                    //ITrack audioTrack = timeline.AddAudioGroup().AddTrack();
-
-                    //IClip audio = audioTrack.AddAudio(@textBox1.Text, 0, videoTrack.Duration);
-
-                    // render our slideshow out to a windows media file
-                    //string path = Environment.GetFolderPath(Environment.SpecialFolder.Programs);
-
-                    using (WindowsMediaRenderer renderer = new WindowsMediaRenderer(timeline, outputFile, WindowsMediaProfiles.HighQualityVideo))
-                    {
-                        renderer.Render();
-
-                    }
-                   
-                    //string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
-                    // axWindowsMediaPlayer1.URL = @"C:\Users\dell\Documents\My Dropbox\Deema\Deema\bin\Release\" + outputFile;
-
-                }
-                #endregion
-            });
+            //t.Start();
+            // return t;
         }
 
     }
